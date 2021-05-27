5c798e9681701e2f6664a1c7026001a13aea3e06
atharvakute <a-kute@purdue.edu>
2021-05-27 15:13:08 -0400

    make it better

--- app.py
+++ app.py
@@ -9,6 +9,8 @@ from FaceComparison import compute_facial_datapoints
 from scipy.spatial.distance import cosine
 import os
 import cv2
+from PIL import Image
+from numpy import asarray
 
 app = Flask(__name__)
 
@@ -17,8 +19,105 @@ app.config['SEND_FILE_MAX_AGE_DEFAULT'] = 1
 UPLOAD_FOLDER = 'trial/'
 ALLOWED_EXTENSIONS = {'txt', 'pdf', 'png', 'jpg', 'jpeg', 'gif'}
 app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
+
+
+
 #
 
+
+def store_image_url(image_url, file_destination):
+    with urllib.request.urlopen(image_url) as resource:
+        with open(file_destination,'wb') as filewriter:
+            filewriter.write(resource.read())
+
+def draw_box(image_path, faces, number):
+    #
+    img1 = cv2.imread(image_path)
+
+
+    for face in faces:
+        x, y, w, h = face['box']
+        cv2.rectangle(img1, (x, y), (x + w, y + h), (0, 0, 255), 2)
+
+
+
+    #
+
+
+    # image = plt.imread(image_path)
+    # plt.imshow(image)
+    # ax = plt.gca()
+    # for face in faces:
+    #     x,y,w,h = face['box']
+    #     face_border = Rectangle((x,y),w,h,fill=False,color='red')
+    #     ax.add_patch(face_border)
+    #
+    # plt.axis('off')
+    if number==1:
+        cv2.imwrite('static/a1.jpg', img1)
+    else:
+        cv2.imwrite('static/a2.jpg', img1)
+    plt.clf()
+
+
+def prep_image(image_path, required_size = (224,224)):
+    from mtcnn.mtcnn import MTCNN
+
+    # set the base width of the result
+    basewidth = 300
+    img = Image.open(image_path)
+    # determining the height ratio
+
+    # resize image and save
+    img = img.resize((640, 480), Image.ANTIALIAS)
+    img.save(image_path)
+    image = plt.imread(image_path)
+
+
+
+    # face_image = Image.fromarray(face_boundary)
+    # face_image = face_image.resize(required_size)
+    # face_array = asarray(face_image)
+    # face_images.append(face_array)
+
+
+
+    MTCNN = MTCNN()
+    faces = MTCNN.detect_faces(image)
+    face_images = []
+    uno = image.shape[0]
+    dos = image.shape[1]
+
+
+    for face in faces:
+        x1,y1,w1,h1 = face['box']
+        x2,y2 = x1+w1,y1+h1
+
+
+        if x2>dos:
+           x2=dos
+        if y2>uno:
+            y2=uno
+        if x1<0:
+            x1=0
+
+        print(x1)
+        print(x2)
+        print(y1)
+        print(y2)
+
+        face_boundary = image[y1:y2,x1:x2]
+        face_image = Image.fromarray(face_boundary)
+        face_image = face_image.resize(required_size)
+        face_array = asarray(face_image)
+        face_images.append(face_array)
+
+    return face_images
+
+
+
+
+#
 from mtcnn.mtcnn import MTCNN
 MTCNN = MTCNN()
 
